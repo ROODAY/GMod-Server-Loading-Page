e58944c650c02d529b19bdbb814cd4233c437930
Rudhra Raveendran <rudytheninja@gmail.com>
2020-04-30 13:01:52 -0400

    Updates

---
+++ precache-manifest.245f784d08fc14378041f1bdbde17dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcaf3bd502ce6bb95891b67d8b70676",
+    "url": "/GMod-Server-Loading-Page/index.html"
+  },
+  {
+    "revision": "c6b1409340791336ce89",
+    "url": "/GMod-Server-Loading-Page/static/css/main.780039d9.chunk.css"
+  },
+  {
+    "revision": "59c694e9288932f3e148",
+    "url": "/GMod-Server-Loading-Page/static/js/2.6f61c721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GMod-Server-Loading-Page/static/js/2.6f61c721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b1409340791336ce89",
+    "url": "/GMod-Server-Loading-Page/static/js/main.1e2e0f82.chunk.js"
+  },
+  {
+    "revision": "bdbf7fc1b2024427e25c",
+    "url": "/GMod-Server-Loading-Page/static/js/runtime-main.5a6f9280.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.502215572062cb09ed34d6407e1ba7ae.js"
+  "/GMod-Server-Loading-Page/precache-manifest.245f784d08fc14378041f1bdbde17dfd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GMod-Server-Loading-Page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
