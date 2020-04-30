1c157d18b44336e9b606679a579c1cfe9bc60ad7
Rudhra Raveendran <rudytheninja@gmail.com>
2020-04-30 13:28:17 -0400

    lets try this one

--- app/scripts/demo.js
+++ app/scripts/demo.js
@@ -4,50 +4,141 @@
 /**
  * Start calling loading screen functions when all assets have finished loading.
  */
-contentLoaded(window, function () {
-    'use strict';
+contentLoaded(window, function() {
+  "use strict";
 
-    /**
-     * Loading screen event fixture. This contains an array of actual functions
-     * called while connecting to a server with the associated time.
-     *
-     * @type {Array}
-     */
-    var fixture = [
-        {"func":"GameDetails","args":["[cG] 24/7 TTT Minecraft/Dolls | SpecDM | LowGrav | PointShop","st.compactgamers.com/loading/garrysmod/ttt/bg-pointshop.php?steamid=76561197991989781","ttt_minecraftcity_v4_dark",40,"76561197991989781","terrortown"],"time":854},
-        {"func":"DownloadingFile","args":["maps/ttt_minecraftcity_v4_dark.bsp"],"time":1972},
-        {"func":"SetFilesNeeded","args":[21],"time":2435},
-        {"func":"SetFilesTotal","args":[73],"time":2436},
-        {"func":"DownloadingFile","args":["maps/graphs/ttt_minecraftcity_v4_dark.ain"],"time":9380},
-        {"func":"DownloadingFile","args":["materials/vgui/cg/cg_ps_logo.png"],"time":9621},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/flash.png"],"time":9853},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/full.png"],"time":9980},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_ten.png"],"time":10107},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_nine.png"],"time":10235},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_eight.png"],"time":10356},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_seven.png"],"time":10484},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_six.png"],"time":10611},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_five.png"],"time":10737},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_four.png"],"time":10864},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_thrice.png"],"time":10987},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_twice.png"],"time":11115},
-        {"func":"DownloadingFile","args":["materials/nook/heart_hud/horizontal_heart/hurt_once.png"],"time":11240},
-        {"func":"DownloadingFile","args":["materials/vgui/ttt/icon_melon_laun.png"],"time":11366},
-        {"func":"DownloadingFile","args":["materials/vgui/ttt/icon_poison_dart.png"],"time":11496},
-        {"func":"DownloadingFile","args":["materials/vgui/ttt/icon_cloak.png"],"time":11617},
-        {"func":"DownloadingFile","args":["materials/vgui/ttt/icon_crabpod.png"],"time":11747},
-        {"func":"DownloadingFile","args":["sound/siege/leeroy.mp3"],"time":11871},
-        {"func":"DownloadingFile","args":["materials/tripmine/icon_tripwire.png"],"time":12000},
-        {"func":"SetStatusChanged","args":["Sending client info..."],"time":17669}
-    ];
+  /**
+   * Loading screen event fixture. This contains an array of actual functions
+   * called while connecting to a server with the associated time.
+   *
+   * @type {Array}
+   */
+  var fixture = [
+    {
+      func: "GameDetails",
+      args: [
+        "[cG] 24/7 TTT Minecraft/Dolls | SpecDM | LowGrav | PointShop",
+        "st.compactgamers.com/loading/garrysmod/ttt/bg-pointshop.php?steamid=76561197991989781",
+        "ttt_minecraftcity_v4_dark",
+        40,
+        "76561197991989781",
+        "terrortown"
+      ],
+      time: 854
+    },
+    {
+      func: "DownloadingFile",
+      args: ["maps/ttt_minecraftcity_v4_dark.bsp"],
+      time: 1972
+    },
+    { func: "SetFilesNeeded", args: [21], time: 2435 },
+    { func: "SetFilesTotal", args: [73], time: 2436 },
+    {
+      func: "DownloadingFile",
+      args: ["maps/graphs/ttt_minecraftcity_v4_dark.ain"],
+      time: 9380
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/vgui/cg/cg_ps_logo.png"],
+      time: 9621
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/flash.png"],
+      time: 9853
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/full.png"],
+      time: 9980
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_ten.png"],
+      time: 10107
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_nine.png"],
+      time: 10235
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_eight.png"],
+      time: 10356
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_seven.png"],
+      time: 10484
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_six.png"],
+      time: 10611
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_five.png"],
+      time: 10737
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_four.png"],
+      time: 10864
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_thrice.png"],
+      time: 10987
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_twice.png"],
+      time: 11115
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/nook/heart_hud/horizontal_heart/hurt_once.png"],
+      time: 11240
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/vgui/ttt/icon_melon_laun.png"],
+      time: 11366
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/vgui/ttt/icon_poison_dart.png"],
+      time: 11496
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/vgui/ttt/icon_cloak.png"],
+      time: 11617
+    },
+    {
+      func: "DownloadingFile",
+      args: ["materials/vgui/ttt/icon_crabpod.png"],
+      time: 11747
+    },
+    { func: "DownloadingFile", args: ["sound/siege/leeroy.mp3"], time: 11871 },
+    {
+      func: "DownloadingFile",
+      args: ["materials/tripmine/icon_tripwire.png"],
+      time: 12000
+    },
+    { func: "SetStatusChanged", args: ["Sending client info..."], time: 17669 }
+  ];
 
-    // Emit all loading screen events over time
-    [].forEach.call(fixture, function (loadEvent) {
-        var func = window[loadEvent.func];
-        if (func !== undefined) {
-            setTimeout(function () {
-                func.apply(window, loadEvent.args);
-            }, loadEvent.time);
-        }
-    });
+  // Emit all loading screen events over time
+  [].forEach.call(fixture, function(loadEvent) {
+    var func = window[loadEvent.func];
+    if (func !== undefined) {
+      setTimeout(function() {
+        func.apply(window, loadEvent.args);
+      }, loadEvent.time);
+    }
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,112 @@
 /* jshint node:true */
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var gulp = require("gulp");
+var $ = require("gulp-load-plugins")();
 
-gulp.task('styles', function () {
-  return gulp.src('app/styles/main.scss')
-    .pipe($.plumber())
-    .pipe($.sass({
-      style: 'expanded',
-      precision: 10
-    }))
-    // GMod Awesomium uses Chrome v18
-    .pipe($.autoprefixer({
-      browsers: ['Chrome >= 18', 'last 1 version']
-    }))
-    .pipe(gulp.dest('.tmp/styles'));
+gulp.task("styles", function() {
+  return (
+    gulp
+      .src("app/styles/main.scss")
+      .pipe($.plumber())
+      .pipe(
+        $.sass({
+          style: "expanded",
+          precision: 10
+        })
+      )
+      .pipe(gulp.dest(".tmp/styles"))
+  );
 });
 
-gulp.task('jshint', function () {
-  return gulp.src('app/scripts/**/*.js')
+gulp.task("jshint", function() {
+  return gulp
+    .src("app/scripts/**/*.js")
     .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
+    .pipe($.jshint.reporter("jshint-stylish"))
+    .pipe($.jshint.reporter("fail"));
 });
 
-gulp.task('html', ['styles'], function () {
-  return gulp.src('app/*.html')
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.csso()))
-    .pipe($.useref({searchPath: '{.tmp,app}'}))
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
-    .pipe(gulp.dest('dist'));
+gulp.task("html", gulp.series("styles", function() {
+  return gulp
+    .src("app/*.html")
+    .pipe($.if("*.js", $.uglify()))
+    .pipe($.if("*.css", $.csso()))
+    .pipe($.useref({ searchPath: "{.tmp,app}" }))
+    .pipe($.if("*.html", $.minifyHtml({ conditionals: true, loose: true })))
+    .pipe(gulp.dest("dist"));
+}));
+
+gulp.task("images", function() {
+  return gulp.src("app/images/**/*").pipe(gulp.dest("dist/images"));
 });
 
-gulp.task('images', function () {
-  return gulp.src('app/images/**/*')
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('fonts', function () {
-  return gulp.src('app/fonts/**/*')
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+gulp.task("fonts", function() {
+  return gulp
+    .src("app/fonts/**/*")
+    .pipe($.filter("**/*.{eot,svg,ttf,woff}"))
     .pipe($.flatten())
-    .pipe(gulp.dest('dist/fonts'));
+    .pipe(gulp.dest("dist/fonts"));
 });
 
-gulp.task('extras', function () {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html',
-    'node_modules/apache-server-configs/dist/.htaccess'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
+gulp.task("extras", function() {
+  return gulp
+    .src(
+      [
+        "app/*.*",
+        "!app/*.html",
+        "node_modules/apache-server-configs/dist/.htaccess"
+      ],
+      {
+        dot: true
+      }
+    )
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+gulp.task("clean", require("del").bind(null, [".tmp", "dist"]));
 
-gulp.task('connect', ['styles'], function () {
-  var serveStatic = require('serve-static');
-  var serveIndex = require('serve-index');
-  var app = require('connect')()
-    .use(require('connect-livereload')({port: 35729}))
-    .use(serveStatic('.tmp'))
-    .use(serveStatic('app'))
-    .use(serveIndex('app'));
+gulp.task("connect", gulp.series("styles", function() {
+  var serveStatic = require("serve-static");
+  var serveIndex = require("serve-index");
+  var app = require("connect")()
+    .use(require("connect-livereload")({ port: 35729 }))
+    .use(serveStatic(".tmp"))
+    .use(serveStatic("app"))
+    .use(serveIndex("app"));
 
-  require('http').createServer(app)
+  require("http")
+    .createServer(app)
     .listen(9000)
-    .on('listening', function () {
-      console.log('Started connect web server on http://localhost:9000');
+    .on("listening", function() {
+      console.log("Started connect web server on http://localhost:9000");
     });
-});
+}));
 
-gulp.task('serve', ['connect', 'watch'], function () {
-  require('opn')('http://localhost:9000');
-});
-
-gulp.task('watch', ['connect'], function () {
+gulp.task("watch", gulp.series("connect", function() {
   $.livereload.listen();
 
   // watch for changes
-  gulp.watch([
-    'app/*.html',
-    '.tmp/styles/**/*.css',
-    'app/scripts/**/*.js',
-    'app/images/**/*'
-  ]).on('change', $.livereload.changed);
+  gulp
+    .watch([
+      "app/*.html",
+      ".tmp/styles/**/*.css",
+      "app/scripts/**/*.js",
+      "app/images/**/*"
+    ])
+    .on("change", $.livereload.changed);
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-});
+  gulp.watch("app/styles/**/*.scss", ["styles"]);
+}));
 
-gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
+gulp.task("serve", gulp.series("connect", "watch", function() {
+  require("opn")("http://localhost:9000");
+}));
 
-gulp.task('default', ['clean'], function () {
-  gulp.start('build');
-});
+gulp.task("build", gulp.series("jshint", "html", "images", "fonts", "extras", function() {
+  return gulp.src("dist/**/*").pipe($.size({ title: "build", gzip: true }));
+}));
+
+gulp.task("default", gulp.series("clean", function() {
+  gulp.start("build");
+}));
