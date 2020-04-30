4d0c0aa9627ab3d014f928c903049a9f8a0a0422
Rudhra Raveendran <rudytheninja@gmail.com>
2020-04-30 12:46:56 -0400

    Updates

---
+++ precache-manifest.502215572062cb09ed34d6407e1ba7ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1272693c66f89c8cc08821ba1ac2fa8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b1409340791336ce89",
+    "url": "/static/css/main.780039d9.chunk.css"
+  },
+  {
+    "revision": "59c694e9288932f3e148",
+    "url": "/static/js/2.6f61c721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f61c721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b1409340791336ce89",
+    "url": "/static/js/main.1e2e0f82.chunk.js"
+  },
+  {
+    "revision": "2253c74d1cbfc3a9629a",
+    "url": "/static/js/runtime-main.c21a44a8.js"
+  }
+]);
